219a0c8e26b72461ac5ad48517c7a5b9dc3fb335
LonEr <dsorn2@gmail.com>
2025-06-20 05:55:42 +0700

    feat: add new annotation for serialize date time

---
+++ src/main/java/com/brightkut/kei/serializer/LocalDateTimeSerialize.java
@@ -0,0 +1,16 @@
+package com.brightkut.kei.serializer;
+
+import com.fasterxml.jackson.annotation.JacksonAnnotationsInside;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+@JacksonAnnotationsInside
+@JsonSerialize(using = LocalDateTimeSerializer.class)
+public @interface LocalDateTimeSerialize {
+}
